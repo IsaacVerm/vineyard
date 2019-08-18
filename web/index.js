2ea28dbab6a958acce67ef2f4cf878ff0b6ffa82
IsaacVerm <isaacverm@gmail.com>
2019-08-18 21:15:49 +0200

    Update test vue example with spray data

--- web/index.js
+++ web/index.js
@@ -4,6 +4,12 @@ var vm = new Vue({
     spray: '',
   },
   mounted() {
-      this.spray = axios.get('http://127.0.0.1:5000/spray?predictionPercentageThreshold=20&daysNotSprayedThreshold=200')['spray'];
+    this.response = axios
+      .get(
+        'http://127.0.0.1:5000/spray?predictionPercentageThreshold=20&daysNotSprayedThreshold=200',
+      )
+      .then(response => {
+        this.spray = response['data']['spray'];
+      });
   },
 });
